1f2a703a8947bb50b3d6476536d94b03cbd51acd
FrancoisLaumond <laumondfrancois@gmail.com>
2024-03-12 13:53:08 +0100

    commit

--- js.js
+++ js.js
@@ -22,3 +22,8 @@ $closeBtnsArr.forEach(function($btn) {
   });
 });
 
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/guxXtqsLCuDsKYyy/scene.splinecode');
